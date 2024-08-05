ca9dd5ca8e11750f06a5e4fadd36ce08e82088c3
Nikita Sharma <111515819+Nikita110602@users.noreply.github.com>
2024-08-05 16:51:09 +0530

    Update Login.js

--- frontend/src/components/Auth/Login.js
+++ frontend/src/components/Auth/Login.js
@@ -23,7 +23,7 @@ function Login() {
     };
     try {
       setloading(true);
-      const response = await fetch("http://localhost:4000/api/auth/login", {
+      const response = await fetch("https://prodigy-fs-task-02.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(user),
